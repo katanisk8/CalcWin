0f5191d7d43c7d48c531cce34def0c15c43fabb7
Michał Makowej <michal@makowej.pl>
2017-08-13 21:11:42 +0200

    Assemlby

--- CalcWin/Controllers/CalculatorController.cs
+++ CalcWin/Controllers/CalculatorController.cs
@@ -1,9 +1,10 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CalcWin.Data;
+using CalcWin.Models;
 
 namespace CalcWin.Controllers
 {
@@ -14,11 +15,19 @@ namespace CalcWin.Controllers
         public CalculatorController(ApplicationDbContext context) 
         {
             db = context;
+
+            //db.Flavors.Add(new Flavor { Name = "Słodki", Acidity = 6 });
+            //db.Flavors.Add(new Flavor { Name = "Półsłodki", Acidity = 7 });
+            //db.Flavors.Add(new Flavor { Name = "Półwytrawny", Acidity = 9 });
+            //db.Flavors.Add(new Flavor { Name = "Wytrawny", Acidity = 10 });
+
+            //db.SaveChanges();
         }
 
         public IActionResult Index()
         {
-            return View();
+            var model = db.Flavors;
+            return View(model);
         }
     }
 }
