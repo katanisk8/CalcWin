76cfc37c84ad5210b3411bd31e5b6ee985a8027f
Michał Makowej <michal@makowej.pl>
2017-12-23 21:36:05 +0100

    Migration

--- CalcWin/Program.cs
+++ CalcWin/Program.cs
@@ -1,18 +1,24 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace CalcWin
 {
-   public class Program
-   {
-      public static void Main(string[] args)
-      {
-         BuildWebHost(args).Run();
-      }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
 
-      public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-   }
+        public static IWebHost BuildWebHost(string[] args) =>
+              WebHost.CreateDefaultBuilder(args)
+                  .UseStartup<Startup>()
+                  .ConfigureAppConfiguration((hostContext, config) =>
+                  {
+                    config.Sources.Clear();
+                    config.AddJsonFile("appsettings.json", optional: true);
+                  })
+                  .Build();
+    }
 }
--- CalcWin/Startup.cs
+++ CalcWin/Startup.cs
@@ -13,27 +13,12 @@ namespace CalcWin
 {
     public class Startup
     {
-        //public IConfigurationRoot Configuration { get; set; }
-
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-
-            if (env.IsDevelopment())
-            {
-                // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets<Startup>();
-            }
-
-            builder.AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
