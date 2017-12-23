6c31663e20a9ebd26e9dc2abf79b8c14e4e3d522
Michał Makowej <michal@makowej.pl>
2017-12-23 21:01:33 +0100

    Migration

--- CalcWin/Startup.cs
+++ CalcWin/Startup.cs
@@ -11,77 +11,84 @@ using Microsoft.AspNetCore.Identity;
 
 namespace CalcWin
 {
-   public class Startup
-   {
-      //public IConfigurationRoot Configuration { get; set; }
+    public class Startup
+    {
+        //public IConfigurationRoot Configuration { get; set; }
 
-      public Startup(IHostingEnvironment env)
-      {
-         var builder = new ConfigurationBuilder()
-             .SetBasePath(env.ContentRootPath)
-             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-             .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-             .AddEnvironmentVariables();
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
 
-         if (env.IsDevelopment())
-         {
-            // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
-            builder.AddUserSecrets<Startup>();
-         }
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets<Startup>();
+            }
 
-         builder.AddEnvironmentVariables();
-         Configuration = builder.Build();
-      }
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
 
-      public IConfigurationRoot Configuration { get; }
+        public IConfigurationRoot Configuration { get; }
 
-      // This method gets called by the runtime. Use this method to add services to the container.
-      public void ConfigureServices(IServiceCollection services)
-      {
-         // Add framework services.
-         services.AddDbContext<ApplicationDbContext>(options =>
-             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-         services.AddIdentity<ApplicationUser, IdentityRole>()
-             .AddEntityFrameworkStores<ApplicationDbContext>()
-             .AddDefaultTokenProviders();
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
 
-         services.AddMvc();
+            services.Configure<IISOptions>(options =>
+            {
+                options.AuthenticationDisplayName = "ss";
+                options.AutomaticAuthentication = true;
+                options.ForwardClientCertificate = true;
+            });
 
-         // Add application services.
-         services.AddTransient<IEmailSender, AuthMessageSender>();
-         services.AddTransient<ISmsSender, AuthMessageSender>();
-      }
+            services.AddMvc();
 
-      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-      public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-      {
-         loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-         loggerFactory.AddDebug();
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+        }
 
-         if (env.IsDevelopment())
-         {
-            app.UseDeveloperExceptionPage();
-            app.UseDatabaseErrorPage();
-            //app.UseBrowserLink();
-         }
-         else
-         {
-            app.UseExceptionHandler("/Home/Error");
-         }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
 
-         app.UseStaticFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                //app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
-         app.UseAuthentication();
+            app.UseStaticFiles();
 
-         // Add external authentication middleware below. To configure them please see https://go.microsoft.com/fwlink/?LinkID=532715
+            app.UseAuthentication();
 
-         app.UseMvc(routes =>
-         {
-            routes.MapRoute(
-                   name: "default",
-                   template: "{controller=Home}/{action=Index}/{id?}");
-         });
-      }
-   }
+            // Add external authentication middleware below. To configure them please see https://go.microsoft.com/fwlink/?LinkID=532715
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
 }
