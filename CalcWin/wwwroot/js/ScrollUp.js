b3bef469285ec06e2301d5c33d41124cb2b98ac3
Michał Makowej <michal@makowej.pl>
2017-12-25 10:50:07 +0100

    javascript

---
+++ CalcWin/wwwroot/js/ScrollUp.js
@@ -0,0 +1,35 @@
+﻿<script>
+    function animateScrollTop(target, duration) {
+        duration = duration || 16;
+
+    var $window = $(window);
+        var scrollTopProxy = {value: $window.scrollTop() };
+        var expectedScrollTop = scrollTopProxy.value;
+
+        if (scrollTopProxy.value != target) {
+        $(scrollTopProxy).animate(
+            { value: target },
+            {
+                duration: duration,
+
+                step: function (stepValue) {
+                    var roundedValue = Math.round(stepValue);
+                    if ($window.scrollTop() !== expectedScrollTop) {
+                        $(scrollTopProxy).stop();
+                    }
+                    $window.scrollTop(roundedValue);
+                    expectedScrollTop = roundedValue;
+                },
+
+                complete: function () {
+                    if ($window.scrollTop() != target) {
+                        setTimeout(function () {
+                            animateScrollTop(target);
+                        }, 16);
+                    }
+                }
+            }
+        );
+    }
+    }
+</script>
