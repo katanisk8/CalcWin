101416035b1515643bea9adea906e29783c1b730
Michał Makowej <michal@makowej.pl>
2018-01-22 15:22:06 +0100

    fff

--- CalcWin/BusinessLogic/ControllersLogic/CalculatorLogic.cs
+++ CalcWin/BusinessLogic/ControllersLogic/CalculatorLogic.cs
@@ -8,81 +8,82 @@ using CalcWin.BusinessLogic.ControllersValidations;
 
 namespace CalcWin.BusinessLogic
 {
-    public class CalculatorLogic
-    {
-        private readonly ApplicationDbContext db;
-        private CalculatorValidation validation;
+   public class CalculatorLogic
+   {
+      private readonly ApplicationDbContext db;
+      private CalculatorValidation validation;
 
-        public CalculatorLogic(ApplicationDbContext context)
-        {
-            db = context;
-            validation = new CalculatorValidation();
-        }
+      public CalculatorLogic(ApplicationDbContext context)
+      {
+         db = context;
+         validation = new CalculatorValidation();
+      }
 
-        public CalculatorViewModel PrepareStartData()
-        {
-            CalculatorViewModel viewModel = new CalculatorViewModel();
-            List<Ingredient> fruits = new List<Ingredient>();
+      public CalculatorViewModel PrepareStartData()
+      {
+         CalculatorViewModel viewModel = new CalculatorViewModel { Result = new Result { Mixture = new Mixture(), Recipe = new Recipe { Ingredients = new List<Ingredient>() }, Wine = new Wine() } };
 
-            foreach (var fruit in db.Fruits.ToList())
-            {
-                fruits.Add(
-                    new Ingredient
-                    {
-                        Fruit = fruit
-                    }
-                );
-            }
+         List<Ingredient> fruits = new List<Ingredient>();
 
-            viewModel.Ingredients = fruits;
-            viewModel.Flavors = db.Flavors.ToList();
-            
-            return viewModel;
-        }
-
-        public void AddWineProject(string userId, CalculatorViewModel model)
-        {
-            if (validation.ValidateAddWineProject(model))
-            {
-                WineProject wineProject = new WineProject();
-                List<Ingredient> ingredients = new List<Ingredient>();
-
-                wineProject.User = userId;
-
-                foreach (var ingredient in model.Ingredients)
+         foreach (var fruit in db.Fruits.ToList())
+         {
+            fruits.Add(
+                new Ingredient
                 {
-                    if (ingredient.Quantity > 0)
-                    {
-                        ingredient.Fruit = db.Fruits.First(x => x.Id == ingredient.Fruit.Id);
-                        ingredient.Project = wineProject;
-                        ingredients.Add(ingredient);
-                    }
+                   Fruit = fruit
                 }
+            );
+         }
 
-                wineProject.Ingredients = ingredients;
-                wineProject.Name = model.Name;
-                wineProject.Flavor = db.Flavors.First(x => x.Id == model.SelectedFlavor);
-                wineProject.AlcoholQuantity = model.SelectedAlcoholQuantity;
-                wineProject.Date = DateTime.Now;
+         viewModel.Ingredients = fruits;
+         viewModel.Flavors = db.Flavors.ToList();
 
-                db.Projects.Add(wineProject);
-                db.SaveChanges();
-            }
-        }
+         return viewModel;
+      }
 
-        public CalculatorViewModel PrepareWineResult(CalculatorViewModel model)
-        {
-            if (model != null)
+      public void AddWineProject(string userId, CalculatorViewModel model)
+      {
+         if (validation.ValidateAddWineProject(model))
+         {
+            WineProject wineProject = new WineProject();
+            List<Ingredient> ingredients = new List<Ingredient>();
+
+            wineProject.User = userId;
+
+            foreach (var ingredient in model.Ingredients)
             {
-                List<Ingredient> ingredients = model.Ingredients.Where(x => x.Quantity > 0).ToList();
-                Flavor flavor = db.Flavors.First(x => x.Id == model.SelectedFlavor);
-                double selectedAlcoholQuantity = model.SelectedAlcoholQuantity;
-                double juiceCorretion = model.JuiceCorretion;
-                //Supplements suplements = db.Supplement
-
-
+               if (ingredient.Quantity > 0)
+               {
+                  ingredient.Fruit = db.Fruits.First(x => x.Id == ingredient.Fruit.Id);
+                  ingredient.Project = wineProject;
+                  ingredients.Add(ingredient);
+               }
             }
-            return model; //Calculations.CalculateWine(ingredients, flavor, selectedAlcoholQuantity, juiceCorretion,); ;
-        }
-    }
+
+            wineProject.Ingredients = ingredients;
+            wineProject.Name = model.Name;
+            wineProject.Flavor = db.Flavors.First(x => x.Id == model.SelectedFlavor);
+            wineProject.AlcoholQuantity = model.SelectedAlcoholQuantity;
+            wineProject.Date = DateTime.Now;
+
+            db.Projects.Add(wineProject);
+            db.SaveChanges();
+         }
+      }
+
+      public CalculatorViewModel PrepareWineResult(CalculatorViewModel model)
+      {
+         if (model != null)
+         {
+            List<Ingredient> ingredients = model.Ingredients.Where(x => x.Quantity > 0).ToList();
+            Flavor flavor = db.Flavors.First(x => x.Id == model.SelectedFlavor);
+            double selectedAlcoholQuantity = model.SelectedAlcoholQuantity;
+            double juiceCorretion = model.JuiceCorretion;
+            //Supplements suplements = db.Supplement
+
+
+         }
+         return model; //Calculations.CalculateWine(ingredients, flavor, selectedAlcoholQuantity, juiceCorretion,); ;
+      }
+   }
 }
---
+++ CalcWin/Models/CalculatorViewModels/CalculatorViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Calculator.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace CalcWin.Views.Calculator
+{
+   public class CalculatorViewModel
+   {
+      // Start data
+      public IEnumerable<Ingredient> Ingredients { get; set; }
+      public IEnumerable<Flavor> Flavors { get; set; }
+
+      // Wine Project
+      [Required]
+      [Display(Name = "Project Name")]
+      [StringLength(20, ErrorMessage = "Flavor name cannot be longer than 20 characters.")]
+      [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+      public string Name { get; set; }
+
+      [Required]
+      public int SelectedFlavor { get; set; }
+
+      [Required]
+      public double SelectedAlcoholQuantity { get; set; }
+
+      [Required]
+      public double JuiceCorretion { get; set; }
+
+      // Calculation results
+      public Result Result { get; set; }
+   }
+}
